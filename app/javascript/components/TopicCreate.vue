c48d3e87a92091703337ccda502577bb1534164e
niclin <bboyceo@hotmail.com>
2018-11-02 16:41:49 +0800

    I'm great

--- app/javascript/components/TopicCreate.vue
+++ app/javascript/components/TopicCreate.vue
@@ -1,9 +1,21 @@
 <template>
   <div>
     <h1>Add New Topics</h1>
-    <input type="text" v-model="topics.title" class="form-control" autofocus="true">
-    <input type="text" v-model="topics.description" class="form-control" autofocus="true">
-    <button @click="addTopic()" class="btn btn-primary">Add Topic</button>
+
+    <div class="field">
+      <label>Title</label>
+      <input type="text" v-model="topics.title" class="form-control" autofocus="true">
+    </div>
+
+    <div class="field">
+      <label>Description</label>
+      <input type="text" v-model="topics.description" class="form-control" autofocus="true">
+
+    </div>
+
+    <div class="actions">
+      <button @click="addTopic()" class="btn btn-primary">Add Topic</button>
+    </div>
 
   </div>
 </template>
